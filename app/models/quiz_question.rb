b9d27e56c6da5aae9e07a9afe93029c25a921bc4
booox <booox@users.noreply.github.com>
2017-11-04 21:05:35 +0800

    change quizzes count

--- app/models/quiz_question.rb
+++ app/models/quiz_question.rb
@@ -1,5 +1,5 @@
 class QuizQuestion < ApplicationRecord
-  belongs_to :quiz, :counter_cache => :questions_count
+  belongs_to :quiz  #, :counter_cache => :questions_count
   belongs_to :question
   # has_many :quiz_details #, class_name: 'QuizDetail', dependent: :destroy
 end
