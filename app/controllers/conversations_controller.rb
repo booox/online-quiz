060ed708186b90560021827e61115039153d27ec
booox <booox@users.noreply.github.com>
2017-11-10 14:02:51 +0800

    Added conversation is_read status can change

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -14,9 +14,7 @@ class ConversationsController < ApplicationController
     @newest_message = @conversation.newest_message
     @first_message = @conversation.first_message
 
-    # @message = @conversation.messages.build
-
-    # cookies.permanent[:"conversation-#{@conversation.id}"] = @conversation.id
+    @newest_message.update(is_read: true)
   end
 
   def reply
@@ -26,14 +24,14 @@ class ConversationsController < ApplicationController
     recipient_id = first_message.recipient_id == current_user.id ? first_message.sender_id : first_message.recipient_id
     message = @conversation.messages.create(sender_id: current_user.id,
                                             recipient_id: recipient_id,
-                                            subject: "回复",
+                                            subject: "吐槽反馈",
                                             content: params[:message][:content])
     if message.save
       notification_user = User.find(recipient_id)
       notification_subject = "新的回复"
       notification_message = "您有新的回复，请到收件箱中查看。"
       Notification.send_notification(notification_user, notification_subject, notification_message)
-      
+
     end
   end
 end
