e90010304a171fa010ec821c18c013115a84b701
booox <booox@users.noreply.github.com>
2017-11-14 12:36:17 +0800

    Modified redis conf

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-Redis.current = Redis.new(:host => ( ENV["REDIS_HOST"] || '127.0.0.1'),
-                          :port => ( ENV["REDIS_PORT"] || '6379').to_i,
-                          :password => (ENV["REDIS_PASSWORD"] || ''))
+Redis.current = Redis.new(:host => ( ENV["REDIS_HOST"] ),
+                          :port => ( ENV["REDIS_PORT"] ).to_i,
+                          :password => (ENV["REDIS_PASSWORD"] ))
