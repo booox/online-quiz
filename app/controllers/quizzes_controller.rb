614ddf52d103aed7ef0830f557407abaad02fbd4
booox <booox@users.noreply.github.com>
2017-11-17 06:58:58 +0800

    Implement quiz statistics with Redis

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -10,7 +10,7 @@ class QuizzesController < ApplicationController
     QuizDetail.where(user_id: current_user, quiz_id: @quiz).destroy_all
 
     Leaderboard.prefixs.each do |prefix|
-      Leaderboard.remove_element("#{prefix}:quiz:#{@quiz.id}", "user:#{@user.id}")
+      Leaderboard.remove_element("#{prefix}:quiz:#{@quiz.id}", "user:#{current_user.id}")
     end
 
     flash[:notice] = "成功清理答题记录"
@@ -19,13 +19,17 @@ class QuizzesController < ApplicationController
 
   def leaderboard
     key = "score:quiz:#{@quiz.id}"
-    @leaders = Leaderboard.top(key).paginate( :page => params[:page], per_page: 10 )
+    @leaders = Leaderboard.top(key).paginate( :page => params[:page], per_page: 35 )
     @current_user_score = Leaderboard.get_user_score(key, current_user).to_i
     @current_user_rank = Leaderboard.get_user_rank(key, current_user)
   end
 
   def statistics
-    @quiz_users = QuizDetail.where(quiz_id: @quiz.id).pluck(:user_id)
+    @quiz_questions_size = @quiz.questions.size
+    statistics_key = "statistics:quiz:#{@quiz.id}"
+
+    @current_user_statistics = Leaderboard.get_user_score(statistics_key, current_user).to_i
+    @statistics = Leaderboard.top(statistics_key).paginate( :page => params[:page], per_page: 35 )
   end
 
   private
